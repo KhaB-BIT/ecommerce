419ab2a4cc3504248812197129406cc15378ce54
Minh Kha Bui <buiminhkha24072001@gmail.com>
2023-05-01 22:26:29 +0700

    logic login and resgister

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -9,13 +9,40 @@ const register = asyncHandler(async (req, res) => {
             mes: "Missing inputs",
         })
     }
-    const response = await User.create(req.body)
-    return res.status(200).json({
-        success: response ? true : false,
-        response,
-    })
+
+    const user = await User.findOne({ email })
+    if (user) {
+        throw new Error("This email have been register")
+    } else {
+        const newUser = await User.create(req.body)
+        return res.status(200).json({
+            success: newUser ? true : false,
+            mes: newUser ? "Register successfully" : "Something wrong",
+        })
+    }
+})
+
+const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            mes: "Missing inputs",
+        })
+    }
+    const response = await User.findOne({ email })
+    if (response && (await response.isCorrectPassword(password))) {
+        const { password, role, ...userData } = response.toObject()
+        return res.status(200).json({
+            success: true,
+            useData: userData,
+        })
+    } else {
+        throw new Error("Invalid email or password")
+    }
 })
 
 module.exports = {
     register,
+    login,
 }
