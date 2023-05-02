ae20938531e50bbece2c2ddac0d6533d5a924267
Minh Kha Bui <buiminhkha24072001@gmail.com>
2023-05-02 12:24:25 +0700

    access token and refresh token 1

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,9 @@
 const User = require("../models/user")
 const asyncHandler = require("express-async-handler")
+const {
+    generateAccessToken,
+    generateRefreshToken,
+} = require("../middlewares/jwt")
 
 const register = asyncHandler(async (req, res) => {
     const { email, password, firstname, lastname } = req.body
@@ -35,17 +39,46 @@ const login = asyncHandler(async (req, res) => {
     const response = await User.findOne({ email })
     if (response && (await response.isCorrectPassword(password))) {
         //convert to plant object
+        //slice password and role from response
         const { password, role, ...userData } = response.toObject()
+        //create accessToken
+        const accessToken = generateAccessToken(response._id, role)
+        //create refreshtoken
+        const refreshToken = generateRefreshToken(response._id)
+        //save refreshtoken to db
+        await User.findByIdAndUpdate(
+            response._id,
+            { refreshToken },
+            { new: true }
+        )
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        })
         return res.status(200).json({
             success: true,
-            useData: userData,
+            accessToken,
+            userData,
         })
     } else {
         throw new Error("Invalid email or password")
     }
 })
 
+const getCurrent = asyncHandler(async (req, res) => {
+    const { _id } = req.user
+
+    const user = await User.findById(_id).select(
+        "-refreshToken -password -role"
+    )
+    return res.status(200).json({
+        success: false,
+        result: user ? user : "User not found",
+    })
+})
+
 module.exports = {
     register,
     login,
+    getCurrent,
 }
